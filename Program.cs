d59435868d98f82ab85c859c44cb003f524838ae
shuuichicross <themadcr3at0r@gmail.com>
2017-10-03 18:22:21 +0800

    please check

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace helloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Hello World");
+            Console.Read();
+        }
+    }
+}
